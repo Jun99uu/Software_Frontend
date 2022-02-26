1c1072b47cf4fc12fabc014b03d17fa3d0dc97f8
Jun99uu <igun0423@naver.com>
2022-02-26 15:34:53 +0900

    intro first commit

---
+++ app/src/main/java/com/example/sofront/IntroViewPagerAdapter.kt
@@ -0,0 +1,15 @@
+package com.example.sofront
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class IntroViewPagerAdapter(fa:FragmentActivity) : FragmentStateAdapter(fa){
+    var fragments = listOf<Fragment>() //프래그먼트가 저장될 배열
+
+    override fun getItemCount(): Int = fragments.size //프래그먼트 배열의 크기
+
+    override fun createFragment(position: Int): Fragment {
+        return fragments.get(position); //반환될 프래그먼트
+    }
+}
