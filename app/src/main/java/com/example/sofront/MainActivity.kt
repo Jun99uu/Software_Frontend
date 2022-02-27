643554c1530e1d19b51d4eaa33e9cd481c7b795e
Jun99uu <igun0423@naver.com>
2022-02-28 01:23:27 +0900

    버튼 수정2

--- app/src/main/java/com/example/sofront/MainActivity.kt
+++ app/src/main/java/com/example/sofront/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.sofront.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +15,9 @@ class MainActivity : AppCompatActivity() {
         // intent는 마지막에 다 하고 연결하면 되니까 따로 작성할 필요는 없구
         // 코드 테스트 할 때 intent 사용하면, 깃에 업로드할 때는 그 부분 지우고 push 해주세영
         // 화이팅 (●'◡'●)
+        val fragmentList = listOf(IntroFragment1(), IntroFragment2(), IntroFragment3(), IntroFragment4(), IntroFragment5())
+        val adapter = IntroViewPagerAdapter(this)
+        adapter.fragments = fragmentList
+        binding.intro.adapter = adapter
     }
 }
