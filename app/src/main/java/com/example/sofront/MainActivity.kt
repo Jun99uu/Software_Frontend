032af5ba5a57c0240d7c35867818b13509de803d
parksk99 <parksk99@naver.com>
2022-02-27 23:14:39 +0900

    메인액티비티에 Intent지우기

--- app/src/main/java/com/example/sofront/MainActivity.kt
+++ app/src/main/java/com/example/sofront/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         // intent는 마지막에 다 하고 연결하면 되니까 따로 작성할 필요는 없구
         // 코드 테스트 할 때 intent 사용하면, 깃에 업로드할 때는 그 부분 지우고 push 해주세영
         // 화이팅 (●'◡'●)
-        val myIntent = Intent(this, DetailInfoActivity::class.java)
-        startActivity(myIntent)
+//        val myIntent = Intent(this, DetailInfoActivity::class.java)
+//        startActivity(myIntent)
     }
 }
