c1bbaa2aba96ea8accc7a0cb07c584fddd46a7de
parksk99 <parksk99@naver.com>
2022-04-11 00:28:39 +0900

    플랜 추가를 위한 리사이클러뷰 추가

--- app/src/main/java/com/example/sofront/ListFragment.kt
+++ app/src/main/java/com/example/sofront/ListFragment.kt
@@ -5,13 +5,63 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sofront.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
+
+    lateinit var recyclerview:RecyclerView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        val view = binding.root
+        val addPlanBtn = binding.addPlanBtn
+        recyclerview = binding.planRv
+
+        setRecyclerView()
+
+
+        addPlanBtn.setOnClickListener {
+            callSetPlanActivity()
+        }
+
+        return view
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding=null
+    }
+
+    fun setRecyclerView(){
+        //TODO: 서버에서 플랜을 가져와서 리사이클러뷰로 띄워줌
+        //TODO: 리사이클러뷰 drag and drop
+        val adapter = PlanRecyclerViewAdapter()
+        addRecyclerViewList(adapter)
+        setRecyclerViewAdapter(adapter)
+    }
+    fun addRecyclerViewList(adapter:PlanRecyclerViewAdapter){
+        adapter.testList.add("test1")
+        adapter.testList.add("test2")
+    }
+    fun setRecyclerViewAdapter(adapter: PlanRecyclerViewAdapter){
+        recyclerview.adapter = adapter
+        recyclerview.layoutManager = LinearLayoutManager(this.context)
+    }
+
+    fun callSetPlanActivity(){
+        //TODO: plan을 생성하는 액티비티를 호출하고 플랜을 받아옴
+//        addPlanList()
+    }
+    fun addPlanList(plan: Plan){
+        //TODO: 플랜을 recyclerView에 추가
+    }
+
 }
