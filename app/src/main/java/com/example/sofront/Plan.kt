265e4f510f0382d93a31782c9db2fb939ed2acba
parksk99 <parksk99@naver.com>
2022-04-11 15:46:48 +0900

    Plan 클래스 변경 (Workout 아래 Set클래스)

--- app/src/main/java/com/example/sofront/Plan.kt
+++ app/src/main/java/com/example/sofront/Plan.kt
@@ -7,11 +7,10 @@ data class Routine(
     val routine:List<Workout>
 )
 data class Workout(
-    val type:WorkoutType,
-    val set:Int,
+    val name:String,
+    val set:List<Set>
+)
+data class Set(
     val count:Int,
     val weight:Int
-)
-data class WorkoutType(
-    val name:String
 )
