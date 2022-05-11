790b4d2dfdc2d3bebf07b2bad8fbb1d93f3e524d
Jun99uu <igun0423@naver.com>
2022-05-12 01:16:22 +0900

    데이터클래스 추가 / 플랜명 중복검사

--- app/src/main/java/com/example/sofront/Plan.kt
+++ app/src/main/java/com/example/sofront/Plan.kt
@@ -3,11 +3,13 @@ package com.example.sofront
 data class Plan(
     var planName:String,
     var hashTagList:ArrayList<String>,
-    val routineList:ArrayList<Routine> //이게 뷰의 근원
+    var routineList:ArrayList<Routine>, //이게 뷰의 근원
+    var makerUid:String,
+    var liked:Boolean
 )
 
 data class Routine(
-    var isExpanded: Boolean,
+    var isExpanded: Boolean, //토글 애니메이션용 변수
     var workoutList:ArrayList<Workout>
 )
 
@@ -19,4 +21,20 @@ data class Workout(
 data class Set(
     var count:Int,
     var weight:Int
+)
+
+data class planDownload(
+    var planName:String,
+    var downloaderUid:String,
+)
+
+data class planLike(
+    var planName:String,
+    var likerUid:String
+)
+
+data class planComment(
+    var planName:String,
+    var writerUid:String,
+    var commentContent:String
 )
