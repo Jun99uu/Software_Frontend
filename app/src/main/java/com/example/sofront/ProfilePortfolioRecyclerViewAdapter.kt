a0bc4dc384b225addee9344ef6b592f9fe7297d4
parksk99 <parksk99@naver.com>
2022-05-20 17:51:06 +0900

    프로필에서 포트폴리오 뷰 구현 + 눌렀을 때 포트폴리오 액티비티로 인텐트 전환

--- app/src/main/java/com/example/sofront/ProfilePortfolioRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/sofront/ProfilePortfolioRecyclerViewAdapter.kt
@@ -1,28 +1,39 @@
 package com.example.sofront
 
+import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sofront.databinding.PlanRecyclerViewBinding
-import com.example.sofront.databinding.ProfilePortfolioRecyclerViewBinding
+import com.example.sofront.databinding.ProfilePortfolioItemBinding
 
 class ProfilePortfolioRecyclerViewAdapter :
     RecyclerView.Adapter<ProfilePortfolioRecyclerViewAdapter.ViewHolder>() {
     private val portfolioList = mutableListOf<Portfolio>()
-    inner class ViewHolder(binding: ProfilePortfolioRecyclerViewBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolder(private val binding: ProfilePortfolioItemBinding) : RecyclerView.ViewHolder(binding.root) {
         private val title = binding.portfolioTitle
         private val content = binding.portfolioContent
         private val date = binding.portfolioDate
+        private val commentNum = binding.portfolioCommentNum
         fun bind(item : Portfolio){
             title.text= item.title
             content.text = item.content
             date.text = item.date
+            commentNum.text = item.commentList.size.toString()
+            binding.root.setOnClickListener{
+                Log.d("click",binding.portfolioTitle.text.toString())
+                val intent = Intent(binding.root.context,PortfolioActivity::class.java)
+                intent.putExtra("portfolio",item)
+                binding.root.context.startActivity(intent)
+            }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-       val binding = ProfilePortfolioRecyclerViewBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        val binding = ProfilePortfolioItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+
         return ViewHolder(binding)
     }
 
