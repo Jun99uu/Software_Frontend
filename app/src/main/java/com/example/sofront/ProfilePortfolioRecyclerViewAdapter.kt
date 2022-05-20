8f698b0c53b26fd11fb3478a86e5f11f10718ff6
parksk99 <parksk99@naver.com>
2022-05-20 15:09:03 +0900

    프로필 포트폴리오 리사이클러뷰 어댑터 추가

---
+++ app/src/main/java/com/example/sofront/ProfilePortfolioRecyclerViewAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.sofront
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sofront.databinding.PlanRecyclerViewBinding
+import com.example.sofront.databinding.ProfilePortfolioRecyclerViewBinding
+
+class ProfilePortfolioRecyclerViewAdapter :
+    RecyclerView.Adapter<ProfilePortfolioRecyclerViewAdapter.ViewHolder>() {
+    val portfolioList = mutableListOf<Portfolio>()
+    inner class ViewHolder(binding: ProfilePortfolioRecyclerViewBinding) : RecyclerView.ViewHolder(binding.root) {
+        private val title = binding.portfolioTitle
+        private val content = binding.portfolioContent
+        private val date = binding.portfolioDate
+        fun bind(item : Portfolio){
+            title.text= item.title
+            content.text = item.content
+            date.text = item.date
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+       val binding = ProfilePortfolioRecyclerViewBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(portfolioList[position])
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
