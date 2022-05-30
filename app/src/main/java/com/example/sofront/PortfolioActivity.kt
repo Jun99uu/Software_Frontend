b1d675c0416e351ed51b7c6d64ed8b9e5d4abf93
parksk99 <parksk99@naver.com>
2022-05-30 21:57:18 +0900

    포트폴리오 좋아요 완성 + 작성하자마자 삭제하려면 commentId가 없어서 삭제 불가능한 버그 수정

--- app/src/main/java/com/example/sofront/PortfolioActivity.kt
+++ app/src/main/java/com/example/sofront/PortfolioActivity.kt
@@ -72,12 +72,40 @@ class PortfolioActivity : AppCompatActivity() {
 
         binding.portfolioImg.setOnClickListener{
             if(System.currentTimeMillis() - time < 900){
-                    postPortfolioLike(
-                        PortfolioLike(
+                val like = PortfolioLike(
                             portfolio.id.toString(),
                             FirebaseAuth.getInstance().uid.toString(),
-                        ), binding, portfolio.liked
-                    )
+                        )
+
+                RetrofitService.retrofitService.postPortfolioLike(like).enqueue(object : Callback<PortfolioLike>{
+                    override fun onResponse(call: Call<PortfolioLike>, response: Response<PortfolioLike>) {
+                        if(response.isSuccessful){
+                            if(portfolio.liked) {
+                                binding.like.setBackgroundResource(R.drawable.ic_heart)
+                                val likeNum = binding.portfolioLikeNum.text.toString()
+                                binding.portfolioLikeNum.text = (likeNum.toInt() -1).toString()
+                                portfolio.liked = false
+                            }
+                            else {
+                                binding.like.setBackgroundResource(R.drawable.ic_heart_fill)
+                                val likeNum = binding.portfolioLikeNum.text.toString().toInt() +1
+                                binding.portfolioLikeNum.text = likeNum.toString()
+                                portfolio.liked = true
+                            }
+                            Log.d("postPortfolioLike","success")
+                        }
+                        else{
+                            Log.e("postPortfolioLike","success but error")
+                            Log.e("postPortfolioLike error code", response.code().toString())
+                        }
+                    }
+
+                    override fun onFailure(call: Call<PortfolioLike>, t: Throwable) {
+                        Log.e("postPortfolioLike","fail")
+                        Log.e("postPortfolioLike error msg",t.message!!)
+                    }
+
+                })
             }
             else
             time = System.currentTimeMillis()
@@ -99,10 +127,13 @@ class PortfolioActivity : AppCompatActivity() {
             override fun onResponse(call: Call<Comment>, response: Response<Comment>) {
                 if(response.isSuccessful){
                     Log.d("postPortfolioComment test success", response.body().toString())
-                    commentList.add(comment)
+
                     val commentNum = binding.portfolioCommentNum.text.toString().toInt()+1
                     binding.portfolioCommentNum.text = commentNum.toString()
+                    comment.commentN = response.body()?.commentN.toString()
+                    commentList.add(comment)
                     adapter.notifyItemInserted(adapter.itemCount-1)
+
                 }else{
                     Log.e("postPortfolioComment test", "success but something error")
                     Log.e("postPortfolioComment error code",response.code().toString())
@@ -136,35 +167,6 @@ class PortfolioActivity : AppCompatActivity() {
                 Log.d("getPortfolioComment test", "fail")
                 Log.d("왜 오류남", t.message.toString())
             }
-        })
-    }
-    private fun postPortfolioLike(like : PortfolioLike, binding: ActivityPortfolioBinding, liked : Boolean){
-        RetrofitService.retrofitService.postPortfolioLike(like).enqueue(object : Callback<PortfolioLike>{
-            override fun onResponse(call: Call<PortfolioLike>, response: Response<PortfolioLike>) {
-                if(response.isSuccessful){
-                    if(liked) {
-                        binding.like.setBackgroundResource(R.drawable.ic_heart)
-                        val likeNum = binding.portfolioLikeNum.text.toString()
-                        binding.portfolioLikeNum.text = (likeNum.toInt() -1).toString()
-                    }
-                    else {
-                        binding.like.setBackgroundResource(R.drawable.ic_heart_fill)
-                        val likeNum = binding.portfolioLikeNum.text.toString().toInt() +1
-                        binding.portfolioLikeNum.text = likeNum.toString()
-                    }
-                    Log.d("postPortfolioLike","success")
-                }
-                else{
-                    Log.e("postPortfolioLike","success but error")
-                    Log.e("postPortfolioLike error code", response.code().toString())
-                }
-            }
-
-            override fun onFailure(call: Call<PortfolioLike>, t: Throwable) {
-                Log.e("postPortfolioLike","fail")
-                Log.e("postPortfolioLike error msg",t.message!!)
-            }
-
         })
     }
 }
